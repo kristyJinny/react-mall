1df9780a2f2e59fa2b3ea5774031da43e226405e
kristyJinny <kristyjinny@gmail.com>
2024-04-03 02:00:49 +0900

    수정

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -7,8 +7,8 @@ function productReducer(state = initialState, action) {
   switch (type) {
     case "GET_PRODUCT_SUCCESS":
       return { ...state, productList: payload.data };
-    // case "GET_SINGLE_PRODUCT_SUCCESS":
-    //   return { ...state, selectedItem: payload.data };
+    case "GET_SINGLE_PRODUCT_SUCCESS":
+      return { ...state, selectedItem: payload.data };
     default:
       return { ...state };
   }
