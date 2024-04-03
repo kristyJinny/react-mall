a6a78901c2f2b52ad397df0f6219838e87bf56b4
kristyJinny <kristyjinny@gmail.com>
2024-04-03 21:01:29 +0900

    상품 경로 재 수정

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -16,7 +16,7 @@ function getProducts(searchQuery) {
 
 function getProductDetail(id) {
   return async (dispatch) => {
-    let url = `https://my-json-server.typicode.com/kristyJinny/react-mallproducts/${id}`;
+    let url = `https://my-json-server.typicode.com/kristyJinny/react-mall/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     dispatch({ type: "GET_PRODUCT_DETAIL_SUCCESS", payload: { data } });
